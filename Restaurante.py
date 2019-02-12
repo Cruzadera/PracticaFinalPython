4801485a778f02bfd8244770700629e5cb46bf49
Maria <mariamunozcruzado@gmail.com>
2019-02-12 22:17:39 +0100

    La opción de eliminar plato ya funciona

--- Restaurante.py
+++ Restaurante.py
@@ -14,24 +14,29 @@ def agregar_plato():
     lista_ingredientes = input("Introduce los ingredientes que lleva este plato:\n")
     lista_alergenos = input("Introduce los alergenos que puede llevar este plato:\n")
     num_raciones = input("Introduce el número de raciones que se han preparado para este plato:\n")
+
     fichero_escritura_platos = open(ruta_platos, 'a', encoding='utf8')
     # Creamos la linea que queremos añadir al fichero con el método join
     datos_plato = [nombre_plato, lista_ingredientes, lista_alergenos, num_raciones]
     nuevo_plato = separador.join(datos_plato)
     fichero_escritura_platos.writelines(nuevo_plato + "\n")
+
     fichero_escritura_platos.close()
+    print("El plato " + nombre_plato + " se ha añadido correctamente")
 
 
 def eliminar_plato():
     listar_platos()
     # TODO: Controlar que sea un número
-    linea_borrar = int(input("Introduce el número del plato que deseas: \n"))
-    fichero_borrado_plato = open(ruta_platos, 'r+', encoding='utf8')
+    linea_borrar = int(input("Introduce el número del plato que deseas borrar: \n"))
+    lineas_fichero_plato = open(ruta_platos, 'r', encoding='utf8').readlines()
+    fichero_borrado_plato = open(ruta_platos, 'w', encoding='utf8')
     numlines = 0
-    for linea in fichero_borrado_plato.readlines():
+    for linea in lineas_fichero_plato:
         numlines += 1
         if numlines != linea_borrar:
             fichero_borrado_plato.write(linea)
+    print("El plato número " + str(linea_borrar) + " se ha borrado correctamente.")
     fichero_borrado_plato.close()
 
 
